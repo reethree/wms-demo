6451e8061730c98b3d42f5b18e62f61284020959
Reza R Ramadhan <reza@contrivent.com>
2016-09-15 11:25:08 +0700

    Tabel Repository

--- app/Models/TabelsRepository.php
+++ app/Models/TabelsRepository.php
@@ -8,10 +8,8 @@ class TabelsRepository extends EloquentRepositoryAbstract {
  
     public function __construct(Model $Model)
     {
-        $this->Database = $Model;
-        
-        $this->visibleColumns = array('id','name','username','email');
- 
+        $this->Database = $Model;        
+        $this->visibleColumns = array('id','name','username','email'); 
         $this->orderBy = array(array('id', 'asc'), array('name','desc'));
     }
 }
