28b356c7ac37e1b99b46e767d802f1a53650bc31
Reza R Ramadhan <reza@contrivent.com>
2017-01-11 15:27:31 +0700

    Call Center

--- app/Http/Controllers/DefaultController.php
+++ app/Http/Controllers/DefaultController.php
@@ -7,6 +7,8 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
+use Illuminate\Http\Request;
+
 class DefaultController extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
