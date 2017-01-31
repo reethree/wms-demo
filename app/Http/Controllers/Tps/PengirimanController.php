d7acf30f06fa71dbee13d59a16cc478d1e752cf5
Reza R Ramadhan <reza@contrivent.com>
2017-01-31 11:55:09 +0700

    31-01-2017

--- app/Http/Controllers/Tps/PengirimanController.php
+++ app/Http/Controllers/Tps/PengirimanController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
+use Artisaninweb\SoapWrapper\Facades\SoapWrapper;
+
 class PengirimanController extends Controller
 {
     /**
