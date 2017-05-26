3923582166ddbb9fa2b9c928522c7123b0d86539
Reza R Ramadhan <reza@contrivent.com>
2017-05-26 11:30:14 +0700

    26-05-2017

--- app/Http/Routes/Data/VesselRoutes.php
+++ app/Http/Routes/Data/VesselRoutes.php
@@ -10,23 +10,23 @@ Route::group(['prefix' => 'vessel', 'namespace' => 'Data'], function(){
     {
         GridEncoder::encodeRequestedData(new \App\Models\TablesRepository(new App\Models\Vessel()) ,Illuminate\Support\Facades\Request::all());
     });
-//    Route::post('/crud', function()
-//    {
-//        $Eloquent = new \App\Models\Eloquent\EloquentVessel();
-//
-//        switch (Illuminate\Support\Facades\Request::get('oper'))
-//        {
-//          case 'add':
-//            return $Eloquent->create(Illuminate\Support\Facades\Request::except('id', 'oper'));
-//            break;
-//          case 'edit':
-//            return $Eloquent->update(Illuminate\Support\Facades\Request::get('id'), Illuminate\Support\Facades\Request::except('id', 'oper'));
-//            break;
-//          case 'del':
-//            return  $Eloquent->delete(Illuminate\Support\Facades\Request::get('id'));
-//            break;
-//        }
-//    });
+    Route::post('/crud', function()
+    {
+        $Eloquent = new \App\Models\Eloquent\EloquentVessel();
+
+        switch (Illuminate\Support\Facades\Request::get('oper'))
+        {
+          case 'add':
+            return $Eloquent->create(Illuminate\Support\Facades\Request::except('id', 'oper'));
+            break;
+          case 'edit':
+            return $Eloquent->update(Illuminate\Support\Facades\Request::get('id'), Illuminate\Support\Facades\Request::except('id', 'oper'));
+            break;
+          case 'del':
+            return  $Eloquent->delete(Illuminate\Support\Facades\Request::get('id'));
+            break;
+        }
+    });
     Route::get('/view/{id}', [
         'as' => 'vessel-view',
         'uses' => 'VesselController@show'
