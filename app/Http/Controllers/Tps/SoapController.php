d32f9b17076452be0c5cdf583dc1dda6ea4611fc
Reza R Ramadhan <reza@contrivent.com>
2018-02-01 08:40:47 +0700

    01-02-2018

--- app/Http/Controllers/Tps/SoapController.php
+++ app/Http/Controllers/Tps/SoapController.php
@@ -25,38 +25,38 @@ class SoapController extends DefaultController {
         $this->kode = 'PRJP';
     }
     
-//    public function demo()
-//    {
-//        
-//        // Add a new service to the wrapper
-//        SoapWrapper::add(function ($service) {
-//            $service
-//                ->name('currency')
-//                ->wsdl('http://currencyconverter.kowabunga.net/converter.asmx?WSDL')
-//                ->trace(true)                                                   // Optional: (parameter: true/false)
-////                ->header()                                                      // Optional: (parameters: $namespace,$name,$data,$mustunderstand,$actor)
-////                ->customHeader($customHeader)                                   // Optional: (parameters: $customerHeader) Use this to add a custom SoapHeader or extended class                
-////                ->cookie()                                                      // Optional: (parameters: $name,$value)
-////                ->location()                                                    // Optional: (parameter: $location)
-////                ->certificate()                                                 // Optional: (parameter: $certLocation)
-//                ->cache(WSDL_CACHE_NONE)                                        // Optional: Set the WSDL cache
-//                ->options(['login' => 'username', 'password' => 'password']);   // Optional: Set some extra options
-//        });
-//
-//        $data = [
-//            'CurrencyFrom' => 'USD',
-//            'CurrencyTo'   => 'EUR',
-//            'RateDate'     => '2014-06-05',
-//            'Amount'       => '1000'
-//        ];
-//
-//        // Using the added service
-//        \SoapWrapper::service('currency', function ($service) use ($data) {
-////            var_dump($service->getFunctions());
-//            var_dump($service->call('GetConversionAmount', [$data])->GetConversionAmountResult);
-//        });
-//        
-//    }
+    public function demo()
+    {
+        
+        // Add a new service to the wrapper
+        SoapWrapper::add(function ($service) {
+            $service
+                ->name('currency')
+                ->wsdl('http://currencyconverter.kowabunga.net/converter.asmx?WSDL')
+                ->trace(true)                                                   // Optional: (parameter: true/false)
+//                ->header()                                                      // Optional: (parameters: $namespace,$name,$data,$mustunderstand,$actor)
+//                ->customHeader($customHeader)                                   // Optional: (parameters: $customerHeader) Use this to add a custom SoapHeader or extended class                
+//                ->cookie()                                                      // Optional: (parameters: $name,$value)
+//                ->location()                                                    // Optional: (parameter: $location)
+//                ->certificate()                                                 // Optional: (parameter: $certLocation)
+                ->cache(WSDL_CACHE_NONE)                                        // Optional: Set the WSDL cache
+                ->options(['login' => 'username', 'password' => 'password']);   // Optional: Set some extra options
+        });
+
+        $data = [
+            'CurrencyFrom' => 'USD',
+            'CurrencyTo'   => 'EUR',
+            'RateDate'     => '2014-06-05',
+            'Amount'       => '1000'
+        ];
+
+        // Using the added service
+        SoapWrapper::service('currency', function ($service) use ($data) {
+//            var_dump($service->getFunctions());
+            var_dump($service->call('GetConversionAmount', [$data])->GetConversionAmountResult);
+        });
+        
+    }
     
     public function GetResponPLP()
     {
