2405dd1c7e3a94a6b1b7a0a46de04edef0907c6c
Reza R Ramadhan <reza@contrivent.com>
2018-03-08 11:05:55 +0700

    Bug Fixing

--- resources/views/import/lcl/report-container.blade.php
+++ resources/views/import/lcl/report-container.blade.php
@@ -69,7 +69,7 @@
 //            ->addColumn(array('label'=>'Status','index'=>'VALIDASI','width'=>80, 'align'=>'center'))
             ->addColumn(array('label'=>'No. Joborder','index'=>'NoJob', 'width'=>150))
             ->addColumn(array('label'=>'Nama Angkut','index'=>'VESSEL','width'=>160))
-            ->addColumn(array('label'=>'VOY','index'=>'VOY','width'=>100,'align'=>'center','hidden'=>true))
+            ->addColumn(array('label'=>'VOY','index'=>'VOY','width'=>100,'align'=>'center','hidden'=>false))
             ->addColumn(array('label'=>'No. Container','index'=>'NOCONTAINER', 'width'=>150,'align'=>'center'))
             ->addColumn(array('label'=>'Size','index'=>'SIZE', 'width'=>100,'align'=>'center'))
             ->addColumn(array('label'=>'Teus','index'=>'TEUS', 'width'=>100,'align'=>'center','hidden'=>true))
@@ -150,7 +150,8 @@
                 <div class="col-md-2">
                     <select class="form-control select2" id="by" name="year" style="width: 100%;" tabindex="-1" aria-hidden="true">
                         <option value="2016" @if($year == '2016') {{ 'selected' }} @endif>2016</option>
-                        <option value="2017" @if($year == '2017') {{ 'selected' }} @endif>2017</option>                      
+                        <option value="2017" @if($year == '2017') {{ 'selected' }} @endif>2017</option>
+                        <option value="2018" @if($year == '2018') {{ 'selected' }} @endif>2018</option>
                     </select>
                 </div>
                 <div class="col-md-2">
