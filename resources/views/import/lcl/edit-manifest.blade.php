2ea57d21e2387b69580f46279132feb6d5c50d2c
Reza R Ramadhan <reza@contrivent.com>
2018-08-07 15:49:04 +0700

    Update

--- resources/views/import/lcl/edit-manifest.blade.php
+++ resources/views/import/lcl/edit-manifest.blade.php
@@ -265,6 +265,7 @@
       //Bind onClick event to the "Delete" button.
       $('#btn-delete').click(function()
       {
+         if(!confirm('Apakah anda yakin ingin menghapus data ini?')){return false;}
         //Gets the selected row id
         rowid = $('#lclManifestGrid').jqGrid('getGridParam', 'selrow');
         rowdata = $('#lclManifestGrid').getRowData(rowid);
